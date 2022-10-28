b18e86c1b18aab782200b942c9530404602beba9
OmarFaruqueRahat7991 <rahatomarfaruque@gmail.com>
2022-10-28 23:12:40 +0600

    another commit

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -6,7 +6,7 @@ const Blogs = () => {
       <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
         <div>
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            Brand new
+            Work Hard
           </p>
         </div>
         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
@@ -33,87 +33,107 @@ const Blogs = () => {
                 height="24"
               />
             </svg>
-            <span className="relative">The</span>
+            <span className="relative">Hard Work</span>
           </span>{' '}
-          quick, brown fox jumps over a lazy dog
+          Will Pay Off
         </h2>
-        <p className="text-base text-gray-700 md:text-lg">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque rem aperiam, eaque ipsa quae.
-        </p>
       </div>
       <div className="grid max-w-sm gap-5 mb-8 lg:grid-cols-3 sm:mx-auto lg:max-w-full">
         <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-          <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-            20 Nov 2020
-          </p>
+          
           <a
             href="/"
             className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
             aria-label="Read article"
             title="Nori grape silver beet broccoli kombu beet"
           >
-            Nori grape silver beet broccoli kombu beet
+           What is CORS?
           </a>
           <p className="max-w-xs mx-auto mb-2 text-gray-700">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque.
+          Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request.
           </p>
-          <a
-            href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-          >
-            Read more
-          </a>
+          
         </div>
         <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-          <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-            20 Nov 2020
-          </p>
+          
           <a
             href="/"
             className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
             aria-label="Read article"
             title="Well, the way they make shows is, they make one"
           >
-            Well, the way they make shows is, they make one
+            Why are you using Firebase?What other options do you have to implement authentication?
           </a>
-          <p className="max-w-xs mx-auto mb-2 text-gray-700">
-            Some pilots get picked and become television programs. Some don't,
-            become nothing.
-          </p>
-          <a
-            href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
-          >
-            Read more
-          </a>
+          <p>Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together. <br />
+					The alternatives of Firebase are:
+					#Okta
+					#Amazon Cognito
+					#JSON Web Token
+					#MongoDB
+					#Passport
+					</p>
+          
+          
         </div>
         <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-          <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
-            20 Nov 2020
-          </p>
+          
           <a
             href="/"
             className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
             aria-label="Read article"
             title="Pommy ipsum smeg head whizz morris himer due"
           >
-            Pommy ipsum smeg head whizz morris himer due
+            How does the private route work?
           </a>
           <p className="max-w-xs mx-auto mb-2 text-gray-700">
-            Taking the mick middle className bog roll bow ties are cool posh
-            nosh off t'shop, stew and dumps.
+          The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property.Private Routes in React Router (also called Protected Routes) require a user being authorized to visit a route (read: page). So if a user is not authorized for a specific page, they cannot access it. The most common example is authentication in a React application where a user can only access the protected pages when they are authorized (which means in this case being authenticated). Authorization goes beyond authentication though. For example, a user can also have roles and permissions which give a user access to specific areas of the application.
           </p>
+          
+        </div>
+        <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
+          
           <a
             href="/"
-            aria-label=""
-            className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
+            aria-label="Read article"
+            title="Pommy ipsum smeg head whizz morris himer due"
           >
-            Read more
+            What is Node? How does Node work?
           </a>
+          <p className="max-w-xs mx-auto mb-2 text-gray-700">
+          Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine (i.e. V8 engine) and executes JavaScript code outside a web browser, which was designed to build scalable network applications. Node.js lets developers use JavaScript to write command line tools and for server-side scripting—running scripts server-side to produce dynamic web page content before the page is sent to the user's web browser. Consequently, Node.js represents a "JavaScript everywhere" paradigm,[6] unifying web-application development around a single programming language, rather than different languages for server-side and client-side scripts.Node.js has an event-driven architecture capable of asynchronous I/O. These design choices aim to optimize throughput and scalability in web applications with many input/output operations, as well as for real-time Web applications (e.g., real-time communication programs and browser games).
+          </p>
+          
+        </div>
+        <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
+          
+          <a
+            href="/"
+            className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
+            aria-label="Read article"
+            title="Pommy ipsum smeg head whizz morris himer due"
+          >
+            What is Next.Js? 
+          </a>
+          <p className="max-w-xs mx-auto mb-2 text-gray-700">
+          Next.js is an open-source web development framework created by Vercel enabling React-based web applications with server-side rendering and generating static websites. React documentation mentions Next.js among "Recommended Toolchains" advising it to developers as a solution when "Building a server-rendered website with Node.js".[4] Where traditional React apps can only render their content in the client-side browser, Next.js extends this functionality to include applications rendered on the server-side.
+          </p>
+          
+        </div>
+        <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
+          
+          <a
+            href="/"
+            className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
+            aria-label="Read article"
+            title="Pommy ipsum smeg head whizz morris himer due"
+          >
+            What is Redux?
+          </a>
+          <p className="max-w-xs mx-auto mb-2 text-gray-700">
+          Redux is an open-source JavaScript library for managing and centralizing application state. It is most commonly used with libraries such as React or Angular for building user interfaces. Similar to (and inspired by) Facebook's Flux architecture, it was created by Dan Abramov and Andrew Clark. Since mid-2016, the primary maintainers are Mark Erikson and Tim Dorr.
+          </p>
+          
         </div>
       </div>
     </div>
--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -9,22 +9,34 @@ const FAQ = () => {
 		<h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
 		<div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-700">
 			<details>
-				<summary className="py-2 outline-none cursor-pointer focus:underline">Optio maiores eligendi molestiae totam dolores similique?</summary>
+				<summary className="py-2 outline-none cursor-pointer focus:underline"> What is CORS?</summary>
 				<div className="px-4 pb-4">
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde neque in fugiat magni, quas animi enim veritatis deleniti ex. Impedit.</p>
+					<p>Cross-Origin Resource Sharing (CORS) is an HTTP-header based mechanism that allows a server to indicate any origins (domain, scheme, or port) other than its own from which a browser should permit loading resources. CORS also relies on a mechanism by which browsers make a "preflight" request to the server hosting the cross-origin resource, in order to check that the server will permit the actual request. In that preflight, the browser sends headers that indicate the HTTP method and headers that will be used in the actual request.</p>
 				</div>
 			</details>
 			<details>
-				<summary className="py-2 outline-none cursor-pointer focus:underline">Modi dolorem veritatis culpa quos consequuntur beatae itaque excepturi perspiciatis?</summary>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">Why are you using Firebase?What other options do you have to implement authentication?</summary>
 				<div className="px-4 pb-4">
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aspernatur quae, eos explicabo odit minima libero veniam similique quibusdam doloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus?</p>
+					<p>Firebase helps you develop high-quality apps, grow your user base, and earn more money. Each feature works independently, and they work even better together. <br />
+					The alternatives of Firebase are:
+					#Okta
+					#Amazon Cognito
+					#JSON Web Token
+					#MongoDB
+					#Passport
+					</p>
 				</div>
 			</details>
 			<details>
-				<summary className="py-2 outline-none cursor-pointer focus:underline">Magni reprehenderit possimus debitis?</summary>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">How does the private route work?</summary>
 				<div className="px-4 pb-4 space-y-2">
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptates aspernatur dolores in consequatur doloremque inventore reprehenderit, consequuntur perspiciatis architecto.</p>
-					<p>Sed consectetur quod tenetur! Voluptatibus culpa incidunt veritatis velit quasi cupiditate unde eaque! Iure, voluptatibus autem eaque unde possimus quae.</p>
+					<p>The react private route component renders child components ( children ) if the user is logged in. If not logged in the user is redirected to the /login page with the return url passed in the location state property.Private Routes in React Router (also called Protected Routes) require a user being authorized to visit a route (read: page). So if a user is not authorized for a specific page, they cannot access it. The most common example is authentication in a React application where a user can only access the protected pages when they are authorized (which means in this case being authenticated). Authorization goes beyond authentication though. For example, a user can also have roles and permissions which give a user access to specific areas of the application.</p>
+				</div>
+			</details>
+			<details>
+				<summary className="py-2 outline-none cursor-pointer focus:underline">What is Node? How does Node work?</summary>
+				<div className="px-4 pb-4 space-y-2">
+					<p>Node.js is an open-source, cross-platform, back-end JavaScript runtime environment that runs on a JavaScript Engine (i.e. V8 engine) and executes JavaScript code outside a web browser, which was designed to build scalable network applications. Node.js lets developers use JavaScript to write command line tools and for server-side scripting—running scripts server-side to produce dynamic web page content before the page is sent to the user's web browser. Consequently, Node.js represents a "JavaScript everywhere" paradigm,[6] unifying web-application development around a single programming language, rather than different languages for server-side and client-side scripts.Node.js has an event-driven architecture capable of asynchronous I/O. These design choices aim to optimize throughput and scalability in web applications with many input/output operations, as well as for real-time Web applications (e.g., real-time communication programs and browser games).</p>
 				</div>
 			</details>
 		</div>
